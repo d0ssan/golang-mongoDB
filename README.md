318014ee759967fe9427a0ffca7c501182eef486
d0ssan <yerdos29@gmail.com>
2021-05-10 16:22:32 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This repasitory was created to understand MongDB. Links:
+
+1. https://www.mongodb.com/blog/search/Nicolas%20Raboy
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func main() {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://d0ssan:<password>@cluster0.cx05p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err = client.Connect(ctx); err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	if err = client.Ping(ctx, readpref.Primary()); err != nil {
+		log.Fatal(err)
+	}
+
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(databases)
+}
+
+// https://www.mongodb.com/blog/post/quick-start-golang--mongodb--starting-and-setup
